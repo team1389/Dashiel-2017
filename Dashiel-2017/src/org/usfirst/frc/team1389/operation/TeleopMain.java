314935f777228111e8937f5f78bdf0dd4ffd7e13
RaphaelMetz <raphaeljmetz@gmail.com>
2017-12-13 22:02:33 -0500

    Copied status' teleopmain then adapted it for dashiel...still the weird error that doesn't happen on stratus

--- Dashiel-2017/src/org/usfirst/frc/team1389/operation/TeleopMain.java
+++ Dashiel-2017/src/org/usfirst/frc/team1389/operation/TeleopMain.java
@@ -3,48 +3,70 @@ package org.usfirst.frc.team1389.operation;
 import java.util.function.Supplier;
 
 import org.usfirst.frc.team1389.robot.RobotSoftware;
+import org.usfirst.frc.team1389.systems.FancyLightSystem;
 import org.usfirst.frc.team1389.systems.GearIntakeSystem;
-import org.usfirst.frc.team1389.systems.OctoMecanumSystem;
-import org.usfirst.frc.team1389.systems.OctoMecanumSystem.DriveMode;
 import org.usfirst.frc.team1389.systems.TeleopGearIntakeSystem;
 import org.usfirst.frc.team1389.watchers.DebugDash;
 
-import com.ctre.CANTalon.FeedbackDevice;
 import com.team1389.hardware.controls.ControlBoard;
-//import com.ctre.CANTalon.FeedbackDevice;
+import com.team1389.hardware.inputs.software.DigitalIn;
 import com.team1389.system.Subsystem;
 import com.team1389.system.SystemManager;
+import com.team1389.system.drive.OctoMecanumSystem;
+import com.team1389.system.drive.OctoMecanumSystem.DriveMode;
 
+/**
+ * system manager
+ * 
+ * @author Quunii
+ *
+ */
 public class TeleopMain
 {
 	SystemManager manager;
 	ControlBoard controls;
 	RobotSoftware robot;
+	DigitalIn timeRunning;
 
+	/**
+	 * 
+	 * @param robot
+	 *            container of all ohm streams
+	 */
 	public TeleopMain(RobotSoftware robot)
 	{
 		this.robot = robot;
 	}
 
+	/**
+	 * initializes systems, and adds them to a list of systems to update
+	 */
 	public void init()
 	{
 		controls = ControlBoard.getInstance();
-		Subsystem drive = setupDrive();
-		manager = new SystemManager(drive);
+		OctoMecanumSystem drive = setupDrive();
+		GearIntakeSystem gearIntake = setupGearIntake(drive.getDriveModeTracker());
+
+		DebugDash.getInstance().watch( gearIntake);
+		manager = new SystemManager(drive,  gearIntake);
 		manager.init();
-		DebugDash.getInstance()
-				.watch(manager.getSystemWatchables().put(robot.armAngleNoOffset.getWatchable("zeroing angle"),
-						robot.rearLeft.getPositionInput().getWatchable("Left encoder"),
-						robot.rearRight.getPositionInput().getWatchable("Right encoder")));
 	}
 
-	private Subsystem setupDrive()
+	/**
+	 * 
+	 * @return a new OctoMecanumSystem
+	 */
+	private OctoMecanumSystem setupDrive()
 	{
 		return new OctoMecanumSystem(robot.voltageDrive, robot.pistons, robot.gyroInput, controls.driveXAxis(),
 				controls.driveYAxis(), controls.driveYaw(), controls.driveTrim(), controls.driveModeBtn(),
 				controls.driveModifierBtn());
 	}
 
+	/**
+	 * 
+	 * @return a new GearIntakeSystem
+	 */
 	private GearIntakeSystem setupGearIntake(Supplier<DriveMode> driveMode)
 	{
 		TeleopGearIntakeSystem Supplier = new TeleopGearIntakeSystem(robot.armAngle, robot.armVel,
@@ -55,10 +77,19 @@ public class TeleopMain
 		return Supplier;
 	}
 
+	/**
+	 * 
+	 * @return a new ClimberSystem
+	 */
+
+
+	/**
+	 * periodically calls update method in all subsystems
+	 */
 	public void periodic()
 	{
 		manager.update();
-
+	
 	}
 
 }
