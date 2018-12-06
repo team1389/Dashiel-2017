6d6d5f562e95fc0af1ad63bede1143a0330a242f
Quunii <kennethmiuraubw@gmail.com>
2018-12-06 15:39:23 -0500

    updated CANTalon initialization

--- src/org/usfirst/frc/team1389/robot/RobotHardware.java
+++ src/org/usfirst/frc/team1389/robot/RobotHardware.java
@@ -9,7 +9,8 @@ import com.team1389.hardware.registry.Registry;
 import com.team1389.hardware.registry.port_types.CAN;
 
 /**
- * responsible for initializing and storing hardware objects defined in {@link RobotLayout}
+ * responsible for initializing and storing hardware objects defined in
+ * {@link RobotLayout}
  * 
  * @author amind
  * @see RobotLayout
@@ -19,23 +20,22 @@ public class RobotHardware extends RobotLayout {
 
 	/**
 	 * Initializes robot hardware by subsystem. <br>
-	 * note: use this method as an index to show hardware initializations that occur, and to find
-	 * the init code for a particular system's hardware
-	 */	
+	 * note: use this method as an index to show hardware initializations that
+	 * occur, and to find the init code for a particular system's hardware
+	 */
 	protected RobotHardware() {
 		registry = new Registry();
 		pdp = new PDPHardware(new CAN(0), registry);
 		ballIntake = new VictorHardware(inv_BALL_INTAKE_MOTOR, pwm_BALL_INTAKE_MOTOR, registry);
 		climber = new VictorHardware(inv_CLIMBER_MOTOR, pwm_CLIMBER_MOTOR, registry);
-		gyro = new GyroHardware<>(GyroHardware.ADXRS_450,spi_GyroPort, registry);
+		gyro = new GyroHardware<>(GyroHardware.ADXRS_450, spi_GyroPort, registry);
 		initDriveTrain();
 		initDriveTrainPneumatics();
 		initGearIntake();
 	}
 
 	private void initGearIntake() {
-		armElevator = new CANTalonHardware(inv_ARM_ELEVATOR_MOTOR, sinv_ARM_ELEVATOR_MOTOR, can_ARM_ELEVATOR_MOTOR,
-				registry);
+		armElevator = new CANTalonHardware(inv_ARM_ELEVATOR_MOTOR, can_ARM_ELEVATOR_MOTOR, registry);
 		gearIntake = new VictorHardware(inv_GEAR_INTAKE_MOTOR, pwm_GEAR_INTAKE_MOTOR, registry);
 	}
 
@@ -51,12 +51,11 @@ public class RobotHardware extends RobotLayout {
 	}
 
 	private void initDriveTrain() {
-		frontLeft = new CANTalonHardware(inv_LEFT_FRONT_MOTOR, sinv_LEFT_FRONT_MOTOR, can_LEFT_FRONT_MOTOR, registry);
-		frontRight = new CANTalonHardware(inv_RIGHT_FRONT_MOTOR, sinv_RIGHT_FRONT_MOTOR, can_RIGHT_FRONT_MOTOR,
-				registry);
+		frontLeft = new CANTalonHardware(inv_LEFT_FRONT_MOTOR, can_LEFT_FRONT_MOTOR, registry);
+		frontRight = new CANTalonHardware(inv_RIGHT_FRONT_MOTOR, can_RIGHT_FRONT_MOTOR, registry);
 		rearLeft = new CANTalonHardware(inv_LEFT_REAR_MOTOR, can_LEFT_REAR_MOTOR, registry);
-		rearRight = new CANTalonHardware(inv_RIGHT_REAR_MOTOR, sinv_RIGHT_REAR_MOTOR, can_RIGHT_REAR_MOTOR, registry);
+		rearRight = new CANTalonHardware(inv_RIGHT_REAR_MOTOR, can_RIGHT_REAR_MOTOR, registry);
 
 	}
-	
+
 }
